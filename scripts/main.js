ec692990cd0826f851be2c76ddaa8800e4637e5b
Deepti Raghavan <deeptir@mit.edu>
2018-08-07 19:42:08 +0530

    version 1 of content

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var bioText = "In progress text"
+var bioText = "I am a first year PhD student in computer science at Stanford. My research interests largely focus on systems and networking. This fall, through Stanford's first year rotation program, I am working with Professor Matei Zaharia."
 function load_bio() {
     document.getElementById("bio").innerHTML = bioText;
 }
