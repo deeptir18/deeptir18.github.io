23338c218014c6e5b8ca801de29dbbd8c5dfa47c
Deepti Raghavan <deeptir@mit.edu>
2018-08-07 19:49:13 +0530

    remove javascript

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-var bioText = "I am a first year PhD student in computer science at Stanford. My research interests largely focus on systems and networking. This fall, through Stanford's first year rotation program, I am working with Professor Matei Zaharia."
-function load_bio() {
-    document.getElementById("bio").innerHTML = bioText;
-}
-
-function load() {
-    load_bio();
-}
-window.onLoad = load();
